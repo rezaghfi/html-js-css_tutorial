102c9c21d3baf6005ff868f0fd2add0955ae7929
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-02-02 12:15:05 +0330

    main for logical operator

--- js/main.js
+++ js/main.js
@@ -16,41 +16,40 @@ var isActive = true;
 var isHoliday = false;
 document.write("<hr>" + firstname + " " + lastname);
 document.write("<hr>" + isActive);
+
 // math operators
 x = 10 * 5;
 x = 10 - 9;
 x = 123 / 6;
 x = 56 % 3;
-// x = x and then x+1
-x = x++;
+x = x++; // x = x and then x+1
 document.write("<hr>" + x);
-// x = x+1 
-x = ++x
+x = ++x; // x = x+1
 document.write("<hr>" + x);
 x = x--;
+
 // assignment Operators
-y = 2
-x = y
-x += y
-x -= y
-x *= y
-x /= y
-x %= y
+y = 2;
+x = y;
+x += y;
+x -= y;
+x *= y;
+x /= y;
+x %= y;
+
 // comparison operators
-num = 10
-console.log(num == 8) // outputs false
-console.log(5==10)
-// equal and same type
-console.log(5===10)
-console.log(5!=10)
-// not equal and same type
-console.log(5!==10)
-console.log(10>5)
-console.log(5>=10)
-console.log(5<10)
-console.log(5<=10)
+num = 10;
+console.log(num == 8); // outputs false
+console.log(5 == 10);
+console.log(5 === 10); // equal and same type
+console.log(5 != 10);
+console.log(5 !== 10); // not equal and same type
+console.log(10 > 5);
+console.log(5 >= 10);
+console.log(5 < 10);
+console.log(5 <= 10);
 
-
-
-
-
+// logical operators
+document.write("<hr>" + "logical operators:and && ,or || ,not !");
+varable = (10 > 5) ? "10 greater than 5" : "5 smaller than 10"
+document.write("<hr>" + varable)
