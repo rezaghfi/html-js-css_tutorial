adcc26cc68e431e186a65fb15d2f4d2b5ee0bc55
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-04-06 21:45:21 +0430

    chage folder name

---
+++ samples/click_event/main.js
@@ -0,0 +1,23 @@
+var num = document.getElementById("number").innerHTML
+
+function add(){
+  document.getElementById("number").innerHTML = num++
+}
+
+function sub(){
+  document.getElementById("number").innerHTML = num--
+}
+
+document.getElementById("btn3").onclick = function test(){
+  document.getElementById("test").innerHTML = "your press * button"
+}
+
+document.getElementById("demo").addEventListener("click",demoFunc)
+
+function demoFunc(){
+  document.getElementById("demo").innerHTML = "ana demo"
+}
+
+document.getElementById("hidden").addEventListener("click",function hidden(){
+  document.getElementById("hidden").style.opacity = 0;
+})
