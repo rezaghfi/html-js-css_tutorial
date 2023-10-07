b10e22dad0a9eb58b901a2c722a6f555c17752ac
reza gholamalitabar <reza.gh.fi@gmail.com>
2023-10-07 20:39:03 +0330

    jalase13

---
+++ web1/jalase13/main.js
@@ -0,0 +1,120 @@
+
+
+
+// operator assingment
+x = 2
+
+y = 3
+
+x = y
+
+x = x + y
+x += y
+
+x = x ** y
+
+// operator logical
+
+x = "sunny"
+
+y = "friday"
+
+// if statement
+
+if(x == "sunny" && y == "friday"){
+  console.log("you do vacation")
+}else{
+  console.log("go to home")
+}
+
+if(x == "sunny" || y == "friday"){
+  console.log("hello")
+}
+
+
+x  = 1
+
+y = '1'
+
+y = "1"
+
+if( y == x){
+  console.log("x is equal to y")
+}
+
+if( y === x){
+  console.log("x is equal to y and same data type")
+}
+
+x = false
+
+if(!x){
+  console.log(x)
+}
+x = 2 
+
+y =3
+
+x **= y // x = x ** y
+
+x = 10 
+
+x %= 5
+
+// operator compration > < >= , <= , == , !=
+
+// if statement
+
+
+day = 0
+
+if(day == 0){
+  for(i = 0; i < day; i++){
+    console.log(i)
+  }
+  console.log("sat")
+}else if(day == 1){
+  console.log("sun")
+}else if(day == 2){
+  console.log("mon")
+}else if(day == 3){
+  console.log("tues")
+}else if(day == 4){
+  console.log("wends")
+}else if(day == 5){
+  console.log("thurs")
+}else if(day == 6){
+  console.log("fri")
+}else{
+  console.log("wrong")
+}
+
+// loop iterator 
+// for
+
+const nums = []
+
+console.log("for statement")
+
+length = 10
+for( i = 0; i < length ; i++){
+  nums[i] = i + 1
+  console.log(nums[i])
+}
+
+i = 0
+
+while(i<10){
+  console.log("hello")
+  i++
+}
+
+x = "hasan"
+
+while(x == "hasan"){
+  if(x == "m"){
+    x = "h"
+  }else if(x == "hasan"){
+    x = "m"
+  }
+}
